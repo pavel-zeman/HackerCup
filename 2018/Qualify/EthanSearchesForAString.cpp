0047c54fa830cc31730f1d553b293ccaad1b9022
Pavel Zeman <pavel.zeman@email.cz>
2020-08-29 22:19:35 +0200

    Added 2018 qualification solutions

---
+++ 2018/Qualify/EthanSearchesForAString.cpp
@@ -0,0 +1,135 @@
+// Description
+// https://www.facebook.com/hackercup/problem/1153996538071503/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
+#define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define LLI long long int
+#define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
+
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+char a[4000];
+char b[10000];
+
+bool search(int al, int bl) {
+    int i = 1;
+    int j = 1;
+    s2:
+    if (i > al) return true;
+    if (j > bl) return false;
+    if (a[i - 1] == b[j - 1]) {
+        i++;
+        j++;
+        goto s2;
+    }
+    if (i == 1) {
+        j++;
+        goto s2;
+    }
+    i = 1;
+    goto s2;
+}
+
+int main(void) {
+    int cases = getInt();
+    for (int cc=0;cc<cases;cc++) {
+        int l = getString(a);
+        bool found = false;
+        FORE(i, 1, l) {
+            FOR(j, i) b[j] = a[j];
+            FOR(j, l) b[j + i] = a[j];
+            b[l + i] = 0;
+            if (!search(l, i + l)) {
+                found = true;
+                break;
+            }
+        }
+        printf("Case #%d: %s\n", cc + 1, found ? b : "Impossible");
+    }
+}
